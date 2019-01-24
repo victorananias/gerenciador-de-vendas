82e5e860eba522eed936602f2642981385c33356
victor <victor.ananias8@gmail.com>
2019-01-24 21:49:13 -0200

    salvando v2

--- src/core/Main.java
+++ src/core/Main.java
@@ -5,6 +5,7 @@ import views.Login;
 
 public class Main {
     public static void main(String args[]) {
+        CrunchifyGetPropertyValues properties = new CrunchifyGetPropertyValues();
         new Login().setVisible(true);
     }
 }
