9d2c8c28a1fc5a12c2faea5005f0337839b0235e
victor <victor.ananias8@gmail.com>
2019-03-03 12:45:22 -0300

    demo added

--- src/app/Config.java
+++ src/app/Config.java
@@ -6,7 +6,7 @@ public class Config {
     // public static final String DBNAME = "gerenciamento_vendas";
     // public static final String URL = "jdbc:mysql://localhost:3306/"+DBNAME+"?useSSL=true";
     // public static final String DRIVER_CLASS = "com.mysql.jdbc.Driver";
-    public static final String DBNAME = "vendas.db";
+    public static final String DBNAME = System.getProperty("user.dir")+"/vendas-demo.db";
     public static final String URL = "jdbc:sqlite:" + DBNAME;
     public static final String DRIVER_CLASS = "org.sqlite.JDBC";
     public static final String USER = "";
